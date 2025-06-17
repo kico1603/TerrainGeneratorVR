ca4e62f4089bd8dd948e3a46779fdcaec18033db
Francisco José Ponce Martínez <tecnicobeniel@hotmail.com>
2025-06-17 17:42:19 +0200

    controllers

---
+++ PruebaTecnica/Assets/Scripts/ActionPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ActionPlayer : MonoBehaviour
+{
+
+
+    private void Update()
+    {
+        //if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        //{
+        //    Debug.Log("Gatillo derecho PULSADO (GetDown)");
+        //    OnTriggerOrFist();
+        //}
+    }
+
+    // Implement your logic here
+    private void OnTriggerOrFist()
+    {
+        // Your code goes here
+    }
+}
